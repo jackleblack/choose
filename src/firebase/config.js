51798c0cbb2add42729f56aa58b284f93d595eb4
giuliano.arend <giuliano.arend@kompetentia.fr>
2019-11-14 15:14:36 +0100

    First commit

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAzDfLPDS-eUt0YUaQimITeEKu2AvLOn1M",
+  authDomain: "choose-376e9.firebaseapp.com",
+  databaseURL: "https://choose-376e9.firebaseio.com",
+  projectId: "choose-376e9",
+  storageBucket: "choose-376e9.appspot.com",
+  messagingSenderId: "225535347733",
+  appId: "1:225535347733:web:95d814b7a3abb2bc15121c"
+};
