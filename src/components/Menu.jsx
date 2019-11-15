b70733e205a6aa88a28d1ca2ab584702a14f3c91
giuliano.arend <giuliano.arend@kompetentia.fr>
2019-11-15 16:57:23 +0100

    Menu & nav

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,7 +9,7 @@ const Menu = () => (
             href="#"
             className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2  border-pink-600"
           >
-            <i className="fas fa-tasks pr-0 md:pr-3"></i>
+            <i className="fas fa-tasks pr-0 md:pr-3 text-pink-600"></i>
             <span className="pb-1 md:pb-0 text-xs md:text-base text-white md:text-white block md:inline-block">
               Tasks
             </span>
@@ -20,7 +20,7 @@ const Menu = () => (
             href="#"
             className="block py-1 md:py-3 pl-1 align-middle text-white no-underline hover:text-white border-b-2 border-gray-800 hover:border-blue-500 "
           >
-            <i className="fas fa-chart-area pr-0 md:pr-3 text-blue-600"></i>
+            <i className="fas fa-chart-area pr-0 md:pr-3 "></i>
             <span className="pb-1 md:pb-0 text-xs md:text-base text-gray-60 md:text-gray-400 block md:inline-block">
               Analytics
             </span>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -56,20 +56,20 @@ const Nav = () => {
         <div className="flex w-full pt-2 content-center justify-between md:w-1/3 md:justify-end">
           <ul className="list-reset flex justify-between flex-1 md:flex-none items-center">
             <li className="flex-1 md:flex-none md:mr-3">
-              <a
+              {/* <a
                 className="inline-block py-2 px-4 text-white no-underline"
                 href="#"
               >
                 Active
-              </a>
+              </a> */}
             </li>
             <li className="flex-1 md:flex-none md:mr-3">
-              <a
+              {/* <a
                 className="inline-block text-gray-600 no-underline hover:text-gray-200 hover:text-underline py-2 px-4"
                 href="#"
               >
                 link
-              </a>
+              </a> */}
             </li>
             <li className="flex-1 md:flex-none md:mr-3">
               <div className="relative inline-block">
